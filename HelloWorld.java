bd787621f152ce36a8c0b312abec52d1994e0363
dprzegal <103595841+dprzegal@users.noreply.github.com>
2022-04-14 08:30:13 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("-----*****Hello World & welcome to Jenkins training*****-----");
+		System.out.println("-----*****Hello World - made some changes*****-----");
 	}
 }
